97e022c0c764b92292e0730643fe547e4c67c252
Mira Kabra <mirakabra123@gmail.com>
2024-03-21 14:07:25 -0700

    print x, not objects

--- local_search.py
+++ local_search.py
@@ -64,51 +64,53 @@ def run_local_search(filename):
                     currCost += costDiff
                     isImproved = True
         count += 1
-    return currCost, path
+    pathX = []
+    for i in range(len(path)):
+        pathX.append(path[i].x)    
+    return currCost, pathX
     
 
 
-'''
-if __name__ == "__main__":
 
-    # parse parameters
-    parser = argparse.ArgumentParser(description="Generate graph for local serach.")
-    parser.add_argument("-f", "--f", help="file name: to generate graph from", required=True, type=str)
+# if __name__ == "__main__":
+
+#     # parse parameters
+#     parser = argparse.ArgumentParser(description="Generate graph for local serach.")
+#     parser.add_argument("-f", "--f", help="file name: to generate graph from", required=True, type=str)
    
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    filename = args.f
-    graph = genGraph(filename).tolist()
-    n = len(graph)
-    path = genInitialPath(graph)
+#     filename = args.f
+#     graph = genGraph(filename).tolist()
+#     n = len(graph)
+#     path = genInitialPath(graph)
 
-    currCost = path_cost(path, graph)
-    print("Initial Path cost:", currCost)
-    isImproved = True
-    count = 0
-    while isImproved and count < 1000:
-        isImproved = False
-        for i in range(0, n-1):
-            for j in range(i+1, n):
+#     currCost = path_cost(path, graph)
+#     print("Initial Path cost:", currCost)
+#     isImproved = True
+#     count = 0
+#     while isImproved and count < 1000:
+#         isImproved = False
+#         for i in range(0, n-1):
+#             for j in range(i+1, n):
                 
-                costDiff = distance(path[i], path[j], graph) + distance(path[i+1], path[(j+1)%n], graph) - distance(path[i], path[i+1], graph) - distance(path[j], path[(j+1)%n], graph)
-                # print('distance(path[i], path[j], graph) : ' , distance(path[i], path[j], graph))
-                # print('distance(path[i+1], path[(j+1)%n], graph) : ' , distance(path[i+1], path[(j+1)%n], graph))
-                # print('distance(path[i], path[i+1], graph) : ' , distance(path[i], path[i+1], graph))
-                # print('distance(path[j], path[(j+1)%n], graph) : ' , distance(path[j], path[(j+1)%n], graph))
-                if costDiff < 0:
-                    print('yes')
-                    print('costDiff : ', costDiff)  
-                    path = two_opt_swap(path, graph, i, j)
-                    print('currCost before : ', currCost) 
-                    currCost += costDiff
-                    print('currCost : ', currCost) 
-                    isImproved = True
-        count+=1
-        print('count : ' , count)
-        #print('currCost : ', currCost)    
-    print('best cost : ', currCost)
-    for i in range(len(path)):
-        print(path[i].x, end=' ')
-    #print(path)                
-'''
+#                 costDiff = distance(path[i], path[j], graph) + distance(path[i+1], path[(j+1)%n], graph) - distance(path[i], path[i+1], graph) - distance(path[j], path[(j+1)%n], graph)
+#                 # print('distance(path[i], path[j], graph) : ' , distance(path[i], path[j], graph))
+#                 # print('distance(path[i+1], path[(j+1)%n], graph) : ' , distance(path[i+1], path[(j+1)%n], graph))
+#                 # print('distance(path[i], path[i+1], graph) : ' , distance(path[i], path[i+1], graph))
+#                 # print('distance(path[j], path[(j+1)%n], graph) : ' , distance(path[j], path[(j+1)%n], graph))
+#                 if costDiff < 0:
+#                     print('yes')
+#                     print('costDiff : ', costDiff)  
+#                     path = two_opt_swap(path, graph, i, j)
+#                     print('currCost before : ', currCost) 
+#                     currCost += costDiff
+#                     print('currCost : ', currCost) 
+#                     isImproved = True
+#         count+=1
+#         print('count : ' , count)
+#         #print('currCost : ', currCost)    
+#     print('best cost : ', currCost)
+#     for i in range(len(path)):
+#         print(path[i].x, end=' ')
+#     #print(path)                
